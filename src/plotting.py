dbe63c1d2346a9d870eeb85cc4f4720783cc2ce7
Felix Krause <felix_krause1999@yahoo.de>
2023-11-29 22:17:28 +0100

    add wind power plot to plotting.py

--- src/plotting.py
+++ src/plotting.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from src.params import cutin_windspeed, cutoff_windspeed
 
 
 def plot_energy_produced(step_df):
@@ -46,12 +47,33 @@ def plot_solar_power(step_df, k=200):
     # Plotting solar for step_df
     working_status = [d.get("solar") for d in step_df['actions_adjusting_status']]
     working_status = [v * 100 for v in working_status]
-    plt.scatter(range(k), working_status[:k], color='green', label='working status', alpha=1, s=15)
+    plt.scatter(range(k), working_status[:k], color='green', label='working status', alpha=1, s=12)
     plt.plot(step_df['solar_irradiance'][:k], color='orange', label='solar irradiance', alpha=1)
     plt.plot(step_df['energy_generated_solar'][:k], color='darkred', label='generated solar energy', alpha=1)
     plt.xlabel('Time ($h$)')
     plt.ylabel('Energy ($kWh$) & Solar Irradiance ($W/m^2$)')
     plt.title('Solar Energy Overview')
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+    # Show the plot
+    plt.show()
+
+
+def plot_wind_power(step_df, k=200):
+    plt.rcParams["figure.figsize"] = (8, 5)
+    plt.rcParams['font.size'] = 12
+
+    # Plotting wind for step_df
+    working_status = [d.get("wind") for d in step_df['actions_adjusting_status']]
+    working_status = [v * 5 for v in working_status]
+    plt.scatter(range(k), working_status[:k], color='green', label='working status', alpha=1, s=12)
+    plt.plot(step_df['wind_speed'][:k], color='lightblue', label='wind speed', alpha=1)
+    plt.plot(step_df['energy_generated_wind'][:k], color='darkred', label='generated wind energy', alpha=1)
+    plt.axhline(y=cutoff_windspeed, color='red', linestyle='dotted', label='cutoff wind speed')
+    plt.axhline(y=cutin_windspeed, color='green', linestyle='dotted', label='cutin wind speed')
+    plt.xlabel('Time ($h$)')
+    plt.ylabel('Energy ($kWh$) & Wind Speed ($m/s$)')
+    plt.title('Wind Energy Overview')
     plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
     # Show the plot
